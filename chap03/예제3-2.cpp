040f90a840ba2f07a16ca0fbbbe12cc3d6bd2622
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-09-26 13:23:06 +0900

    chap03

---
+++ chap03/예제3-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char flag;
+    cin >> flag;
+    switch(flag)
+    {
+        case 'k' :
+            cout << "korea 파이팅!\n";
+        case 'm' :
+            cout << "milk\n";
+        default :
+            cout << "end\n";
+    }
+    cout << "fin";
+}
