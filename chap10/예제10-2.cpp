8cdb19a9715a5ca991d1d026090db8ea2caef830
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2022-12-12 17:04:24 +0900

    studying chap10

---
+++ chap10/예제10-2.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+T bigger(T a, T b) {
+    if (a > b)
+        return a;
+    else if (a < b)
+        return b;
+    else {
+        cout << "두수가 같습니다." << endl;
+        return 0;
+    }
+}
+
+int main() {
+    int a = 20, b = 50;
+    char c = 'a', d = 'z';
+    cout << "bigger(20,50)의 결과는 " << bigger(a, b) << endl;
+    cout << "bigger('a','z')의 결과는 " << bigger(c, d) << endl;
+}*/
